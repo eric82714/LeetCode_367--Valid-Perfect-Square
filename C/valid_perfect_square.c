2e967a9ec7968f0f527d88ca54a07e8924716718
eric82714 <0958820714ab@gmail.com>
2020-03-23 08:16:50 +0800

    Create valid_perfect_square.c

---
+++ C/valid_perfect_square.c
@@ -0,0 +1,16 @@
+bool isPerfectSquare(int num){
+    if (num < 2) return true;
+        
+    long x = num / 2, t = x * x;
+        
+    while (t > num) {
+        x /= 2;
+        t = x * x;
+    }
+        
+    for (long i = x; i <= 2 * x; ++i) {
+        if (i * i == num) return true;
+    }
+        
+    return false;
+}
